6a7d8852f27947ff5d69f73caf4328a33ebe55f2
Insaram Alam Siddique <alaminsaram92@gmail.com>
2021-01-14 04:09:06 +0500

    Final Completed

--- recipe/static/recipe/index.js
+++ recipe/static/recipe/index.js
@@ -29,11 +29,11 @@ function RecentRecipes() {
 		recipes.forEach(recipe => {	
 			
 			let col = document.createElement('div');
-			col.className = 'col-lg col-md-6 col-sm';
+			col.className = 'col-lg col-md col-sm-6';
 
 			let recipediv = document.createElement('div');
 			recipediv.className = 'card';
-			recipediv.style = "width: 18rem; margin-bottom: 30px;"
+			recipediv.style = "width: 18rem; margin: 0 0 30px 0;"
 			col.appendChild(recipediv)
 
 			let imgRes = document.createElement('img');
@@ -69,58 +69,7 @@ function RecentRecipes() {
 		)
 }
 
-function LikedRecipes() {
-	fetch('/mostliked')
-	.then(response => response.json())
-	.then(recipes => {
 
-		h4 = document.createElement('h4')
-		h4.textContent = "Most Liked Recipes"
-		document.body.querySelector('#mostliked').appendChild(h4);
-
-		recipes.forEach(recipe => {	
-			
-			let col = document.createElement('div');
-			col.className = 'col-lg col-md-6 col-sm';
-
-			let recipediv = document.createElement('div');
-			recipediv.className = 'card';
-			recipediv.style = "width: 18rem; margin-bottom: 30px;"
-			col.appendChild(recipediv)
-
-			let imgRes = document.createElement('img');
-			imgRes.style = 'width:286px; height:193.81px';
-			imgRes.src = `http://127.0.0.1:8000/images/${recipe.img}`
-			recipediv.appendChild(imgRes)
-
-			let cardBody = document.createElement('div');
-			cardBody.className = 'card-body';
-			recipediv.appendChild(cardBody); 
-
-			let recipeTitle = document.createElement('h5');
-			recipeTitle.className = 'card-title'
-			recipeTitle.textContent = `${recipe.title}`;
-			cardBody.appendChild(recipeTitle);
-
-			let description = document.createElement('p');
-			description.className = 'card-text';
-			description.textContent = `${recipe.description}`;
-			cardBody.appendChild(description);
-
-			let whereTo = document.createElement('a');
-			whereTo.className = 'btn btn-primary';
-			whereTo.href = `/recipe/${recipe.id}`;
-			whereTo.textContent = 'View Recipe'
-			cardBody.appendChild(whereTo);
-
-
-			document.body.querySelector('#mostliked').appendChild(col);
-		}
-							)
-					}	
-		)
-}
 document.addEventListener('DOMContentLoaded', function(){
 	RecentRecipes();
-	LikedRecipes();
 })
